2178bf27f1eb322425deb73b2bbdd0c27b954a49
Zoran Bogoevski <zbogoevski@gmail.com>
2022-10-10 08:15:10 +0200

    Code cleanup

--- src/Htmldom/HtmldomServiceProvider.php
+++ src/Htmldom/HtmldomServiceProvider.php
@@ -1,50 +1,51 @@
 <?php
-    namespace Htmldom;
 
-    use Illuminate\Support\ServiceProvider;
+namespace Htmldom;
 
-    class HtmldomServiceProvider extends ServiceProvider
+use Illuminate\Support\ServiceProvider;
+
+class HtmldomServiceProvider extends ServiceProvider
+{
+    
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+    
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
     {
-
-        /**
-         * Indicates if loading of the provider is deferred.
-         *
-         * @var bool
-         */
-        protected $defer = true;
-
-        /**
-         * Bootstrap the application events.
-         *
-         * @return void
-         */
-        public function boot()
-        {
-            if (method_exists($this, 'package')) {
-                $this->package('htmldom');
-            }
+        if (method_exists($this, 'package')) {
+            $this->package('htmldom');
         }
-
-        /**
-         * Register the service provider.
-         *
-         * @return void
-         */
-        public function register()
-        {
-            $this->app->bind('htmldom', function () {
-                return new Htmldom;
-            });
-        }
-
-        /**
-         * Get the services provided by the provider.
-         *
-         * @return array
-         */
-        public function provides()
-        {
-            return [];
-        }
-
     }
+    
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('htmldom', function () {
+            return new Htmldom;
+        });
+    }
+    
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides(): array
+    {
+        return [];
+    }
+    
+}
